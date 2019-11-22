f89923239c61ea59b90767b8a0444bfbdc0cfe67
Joel Optimiz <joel@optimiz.claims>
2019-11-22 21:00:32 +0100

    Set up Tailwindcss

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import Layout from './Organisms/Layout/Layout';
+import Button from "./Atoms/button";
 
-import './App.css';
 import Logo from './assets/logo.jpeg';
 
 function App() {
   return (
-      <div className="App">  
-       {/* {Remove class and the App.css when real development work stars} */}  
+      <div className="w-3/4 mx-auto h-auto text-center">   
         <Layout>
-          <h1>Hello and Welcome to the YISS Website</h1>
+          <h1 className="text-xl sm:text-3xl font-bold m-5">Hello and Welcome to the YISS Website</h1>
           <img src={Logo} alt='YISS logo'/>
-          <p>Production is ongoing so hand on for the ride</p>
+          <p className="text-md font-semibold m-5 text-red-500">Production is ongoing so hang on for the ride</p>
+
+          {/* Just Testing TailWindcss */}
+          <div className="flex flex-col w-3/4 mx-auto my-12 items-center">
+            <h1>Super cool page</h1>
+            <Button onClick={() => console.log("I was clicked")}>
+              I am YISS
+            </Button>
+          </div>
         </Layout>
       </div>
   );
