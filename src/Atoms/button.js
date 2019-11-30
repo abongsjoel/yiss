1fee548a3ce615d61623b52e9ceb69b63a6202de
Joel Optimiz <joel@optimiz.claims>
2019-11-30 15:26:03 +0100

    Added Button to Welcome Message

--- src/Atoms/button.js
+++ src/Atoms/button.js
@@ -1,12 +1,20 @@
-import React from "react";
+import React from 'react';
 
-export default function Button({ children, ...buttonProps }) {
-  return (
-    <button
-      className="px-2 py-1 rounded-lg bg-green-400 text-green-800 text-xl font-light uppercase shadow-md hover:shadow-lg"
-      {...buttonProps}
-    >
-      {children}
-    </button>
-  );
+const Button = ({text, btnColor}) => {
+
+    let changingStyle = "hover:bg-blue-700 c-main border  border-blue-700 hover:text-white";
+    if (btnColor === "white") {
+        changingStyle = "hover:bg-white text-white border  border-white hover:text-blue-700";
+    }
+    const fixedStyle = "bg-transparent font-semibold py-1 px-8 border hover:border-transparent rounded text-sm";
+    return (
+        <button className={fixedStyle + " " + changingStyle}>{text}</button>
+    );
 }
+
+export default Button;
+
+// "  text-blue-700   py-2 px-4 border border-blue-500 hover:border-transparent rounded"
+
+{/* <button class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white 
+py-2 px-4 border border-blue-500 hover:border-transparent rounded"></button> */}
