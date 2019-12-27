5c34c057435d5ef87f0afffabd1f9add40bce3e6
Joel Optimiz <joel@optimiz.claims>
2019-12-27 21:38:22 +0100

    Hamburger icon color adjustments

--- src/Molecules/Header.js
+++ src/Molecules/Header.js
@@ -9,7 +9,7 @@ const Header = (props) => (
         <div className="h-auto w-20">
             <Img src={Logo} alt="Logo" />
         </div>
-        <svg className="fill-current text-blue-500" height="32" width="32" ><path d="M4 10h24a2 2 0 000-4H4a2 2 0 000 4zm24 4H4a2 2 0 000 4h24a2 2 0 000-4zm0 8H4a2 2 0 000 4h24a2 2 0 000-4z"/></svg>
+        <svg className="fill-current text-blue-700 hover:text-blue-600" height="32" width="32" ><path d="M4 10h24a2 2 0 000-4H4a2 2 0 000 4zm24 4H4a2 2 0 000 4h24a2 2 0 000-4zm0 8H4a2 2 0 000 4h24a2 2 0 000-4z"/></svg>
     </header>
 )
 
