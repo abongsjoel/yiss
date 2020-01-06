f834fb7385e0643d405a809418fd16d7b2761096
Joel Optimiz <joel@optimiz.claims>
2020-01-06 20:25:39 +0100

    Little touches on navigation menu

--- src/Molecules/NavItems.js
+++ src/Molecules/NavItems.js
@@ -1,13 +1,17 @@
 import React from 'react';
 
-const NavItems = ({menu, styles}) => { 
+const NavItems = ({menu, active, styles}) => { 
+    let linkStyle = "inline md:block hover:text-sec-100 focus:outline-none pb-2 hover:border-b-2 hover:border-sec-100";
+    if(active) {
+        linkStyle += " text-sec-100 font-bold border-b-2 border-sec-100";
+    }
     const menuItems = Object.keys(menu)
         .map(menuElement => {
             return (
                 <li key={menuElement} className={styles +" list-none"}>
                     <a 
                         href={menu[menuElement]}
-                        className="block hover:text-sec-100 focus:outline-none md:pb-2 hover:border-b-2 hover:border-sec-100"
+                        className={linkStyle}
                     >
                         {menuElement}
                     </a>
