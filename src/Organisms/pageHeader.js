96c0345d90b18aec9f3d767700687f9daef1faf9
joel <joel@optimiz.claims>
2020-03-06 17:36:18 +0100

    Ability to pass pageName to pageHeader component

--- src/Organisms/pageHeader.js
+++ src/Organisms/pageHeader.js
@@ -4,7 +4,7 @@ import Img from '../Atoms/Img'
 
 import HeroImage from '../assets/images/hero-img.jpg';
 
-const PageHeader = () => (
+const PageHeader = ({ pageName }) => (
     <div className="relative pb-1/5 sm:pb-1/10 md:pb-1/10 xl:pb-1/10">
     {/* <div className="relative pb-full sm:pb-2/3 md:pb-1/2 xl:pb-2/5"> */}
         <Img 
@@ -14,7 +14,7 @@ const PageHeader = () => (
         />
         <div className="absolute flex h-full w-full items-center hm_bg-black tj-container">
             <p className="pt-2 text-white hero-font font-bold text-xl sm:text-2xl md:text-4xl lg:text-6xl">
-                About YISS
+                {pageName}
             </p>
         </div>
     </div>
--- src/Pages/AboutUsPage.js
+++ src/Pages/AboutUsPage.js
@@ -6,7 +6,7 @@ import Aux from '../hoc/Auxilary';
 
 const AboutUsPage = () => (
     <Aux>
-        <PageHeader />
+        <PageHeader pageName="About Yiss" />
         <div className="flex justify-center font-bold text-4xl py-8">
             <p>Hello and Welcome. <br /><br />This is the about us page</p>
         </div>
