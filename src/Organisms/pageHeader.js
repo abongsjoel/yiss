fab8987d3fd4488a91a95cee6c691848c4842203
joel <joel@optimiz.claims>
2020-03-06 17:46:20 +0100

    Increased hero size in pageHeader component

--- src/Organisms/pageHeader.js
+++ src/Organisms/pageHeader.js
@@ -5,15 +5,16 @@ import Img from '../Atoms/Img'
 import HeroImage from '../assets/images/hero-img.jpg';
 
 const PageHeader = ({ pageName }) => (
-    <div className="relative pb-1/5 sm:pb-1/10 md:pb-1/10 xl:pb-1/10">
-    {/* <div className="relative pb-full sm:pb-2/3 md:pb-1/2 xl:pb-2/5"> */}
+    <div className="relative pb-2/5 md:pb-1/5 ">
+    {/* <div className="relative pb-1/5 sm:pb-1/10 md:pb-1/10 xl:pb-1/10"> */}
         <Img 
             src={HeroImage} 
             alt="Young investor's Science Society" 
             styles=" absolute"  
         />
         <div className="absolute flex h-full w-full items-center hm_bg-black tj-container">
-            <p className="pt-2 text-white hero-font font-bold text-xl sm:text-2xl md:text-4xl lg:text-6xl">
+            <p className="pt-2 text-white hero-font font-bold text-3xl sm:text-4xl md:text-5xl">
+            {/* <p className="pt-2 text-white hero-font font-bold text-xl sm:text-2xl md:text-4xl lg:text-6xl"> */}
                 {pageName}
             </p>
         </div>
