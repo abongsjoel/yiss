4ce941b41364845cd96efb10ba6a83ca887d624b
joel <joel@optimiz.claims>
2020-03-06 18:04:04 +0100

    Added Header Image to PageHeader

--- src/Atoms/Img.js
+++ src/Atoms/Img.js
@@ -4,9 +4,8 @@ const Img = ({src, alt, styles}) => (
     <img 
         src={src} 
         alt={alt} 
-        className={"w-full h-full object-cover object-center " +styles} 
+        className={"w-full h-full object-cover " +styles} 
     />
-    // <img src={src} alt={alt} className={"  w-full h-full object-cover object-center " +styles} />
 );
 
 
--- src/Organisms/pageHeader.js
+++ src/Organisms/pageHeader.js
@@ -2,15 +2,15 @@ import React from 'react';
 
 import Img from '../Atoms/Img'
 
-import HeroImage from '../assets/images/hero-img.jpg';
+import HeaderImage from '../assets/images/about-1.jpg';
 
 const PageHeader = ({ pageName }) => (
     <div className="relative pb-2/5 md:pb-1/5 ">
     {/* <div className="relative pb-1/5 sm:pb-1/10 md:pb-1/10 xl:pb-1/10"> */}
         <Img 
-            src={HeroImage} 
+            src={HeaderImage} 
             alt="Young investor's Science Society" 
-            styles=" absolute"  
+            styles="  object-bottom absolute"  
         />
         <div className="absolute flex h-full w-full items-center hm_bg-black tj-container">
             <p className="pt-2 text-white hero-font font-bold text-3xl sm:text-4xl md:text-5xl">
