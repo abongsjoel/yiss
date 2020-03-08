852f75689192223412bfb8fcb1537b0fcf52da9d
joel <joel@optimiz.claims>
2020-03-08 20:55:37 +0100

    Added Comment Heading to Article component

--- src/Molecules/Article.js
+++ src/Molecules/Article.js
@@ -3,6 +3,8 @@ import React from 'react';
 import H1 from '../Atoms/H1';
 import Para from '../Atoms/Para';
 
+import likeIcon from '../assets/icons/like.svg';
+
 import Aux from '../hoc/Auxilary';
 
 const Article = ({title, body, authorPic, authorName, publishedDate}) => {
@@ -19,8 +21,16 @@ const Article = ({title, body, authorPic, authorName, publishedDate}) => {
                     <Para text={authorName} styles='text-sm font-bold' />
                     <p className='text-gray-700 text-sm' >Read <span className="text-sec-100"><a href='/blog'>more posts</a></span> by this author</p> 
                 </div>
-
             </div>
+
+            {/* Comment Section */}
+            <div className="mt-8 flex items-center">
+                <p className="text-gray-700 text-base font-bold pr-4">3 comments</p>
+                <img src={likeIcon} alt="" className="w-4 h-4" />
+                <p className="text-gray-700 text-sm px-1 font-bold">Likes</p>
+                <p className="bg-gray-300 text-xs p-1">20</p>
+            </div>
+            <hr className="mt-1" />
         </Aux>   
     )
 }
