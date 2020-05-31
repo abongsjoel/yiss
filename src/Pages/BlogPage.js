d41c89dfb0b490658590bae327bab499fa1ae910
joel <joel@optimiz.claims>
2020-05-31 05:28:47 +0100

    Axios is better than fetch definitely

--- src/Pages/BlogPage.js
+++ src/Pages/BlogPage.js
@@ -1,5 +1,7 @@
 import React, { useState,useEffect } from 'react';
 
+import axios from 'axios';
+
 import PageHeader from '../Organisms/pageHeader';
 import BlogListing from '../Molecules/BlogListing';
 import HeaderImage from '../assets/images/blog-header.JPG';
@@ -11,22 +13,50 @@ import Aux from '../hoc/Auxilary';
 
 const Blog = () => {
 
-     const [ blogPosts, setBlogPosts ] = useState([])
+     const [ blogPosts, setBlogPosts ] = useState([]);
+     const [ err, setErr] = useState(false);
 
      console.log("Blog is rendered")
 
      useEffect(() => {
-          fetch('https://jsonplaceholder.typicode.com/posts')
-               .then(response => response.json())
-               .then(data => {
-                    setBlogPosts(data)
+
+          /* Using the Fetch API */
+          // fetch('https://jsonplaceholder.typicode.com/posts')
+          //      .then(response => {
+          //           if (response.status !== 200) {
+          //                console.log('Looks like there was a problem. Status Code: ' +
+          //                  response.status);
+          //                  setErr(true);
+          //                return;
+          //           }
+          //           response.json().then(data => {
+          //                setBlogPosts(data);
+          //           })
+          //      })
+          //      .catch(error => {
+          //           setErr(true);
+          //           console.log("Network error most probably: ", error)
+          //      });
+
+
+          /* Using the axios library */
+          axios.get('https://jsonplaceholder.typicode.com/posts')
+               .then(response => {
+                    setBlogPosts(response.data);
+               })
+               .catch(err => {
+                    setErr(true);
                });
      }, [])
 
      let blogListing = <p>Loading...</p>
 
+     if(err) {
+          blogListing = <p> Something went wrong </p>
+     }
+     console.log("The length of the array is : ", blogPosts.length)
      if(blogPosts.length !== 0) {
-          const blogs = blogPosts.slice(0,9);
+          const blogs = blogPosts.slice(0,5);
           blogListing = blogs.map(blogPost => {
                return (
                     <BlogListing 
@@ -39,11 +69,7 @@ const Blog = () => {
                          firstBlog={false}
                     />
                );
-          })
-          
-          
-          
-         
+          })   
      }
 
 
