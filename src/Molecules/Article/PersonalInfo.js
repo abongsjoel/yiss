d948d37208859b3fbe08d2b2c7eeddcb9216de48
joel <joel@optimiz.claims>
2020-06-11 01:36:33 +0100

    Implemented PersonalInfo component

---
+++ src/Molecules/Article/PersonalInfo.js
@@ -0,0 +1,41 @@
+import React from 'react'
+
+import H1 from '../../Atoms/H1';
+import H2 from '../../Atoms/H2';
+import Input from '../../Atoms/Input';
+
+import Aux from '../../hoc/Auxilary';
+
+const PersonalInfo = ({name, nameChanged, email, emailChanged}) => {
+    return (
+        <Aux>
+            <H1 part1="Post A New " part2="Article" />
+            <hr className="my-5" />
+            <H2 text="Personal Information" />
+            <div className="my-5 w-full flex">
+                <div className="w-1/2 mr-2">
+                    <Input 
+                        type = "text"
+                        placeholder = "Enter your name"
+                        label = "Name"
+                        required
+                        initialValue={name}
+                        textChanged={nameChanged}
+                    />
+                </div>
+                <div className="w-1/2 ml-2">
+                    <Input 
+                        type = "text"
+                        placeholder = "Enter your email address"
+                        label = "Email"
+                        hint = "Your email address will not be published"
+                        initialValue={email}
+                        textChanged={emailChanged}
+                    />
+                </div>
+            </div>
+        </Aux>
+    )
+}
+
+export default PersonalInfo
--- src/Pages/AddArticle.js
+++ src/Pages/AddArticle.js
@@ -2,12 +2,10 @@ import React, {useState} from 'react';
 
 import PageHeader from '../Organisms/pageHeader';
 import HeaderImage from '../assets/images/blog-header.JPG';
-import H1 from '../Atoms/H1';
-import H2 from '../Atoms/H2';
-import Input from '../Atoms/Input';
 import Button from '../Atoms/Button';
 
 import Aux from '../hoc/Auxilary';
+import PersonalInfo from '../Molecules/Article/PersonalInfo';
 
 const AddArticle = () => {
 
@@ -18,6 +16,7 @@ const AddArticle = () => {
         const article = {
             author: name,
             email,
+            date: Date.now()
         }
         console.log(article)
     }
@@ -27,31 +26,12 @@ const AddArticle = () => {
             <PageHeader pageName="Blog" headerImage={HeaderImage} className=' object-bottom'  />
             
             <div className="tj-container">
-                <H1 part1="Post A New " part2="Article" />
-                <hr className="my-5" />
-                <H2 text="Personal Information" />
-                <div className="my-5 w-full flex">
-                    <div className="w-1/2 mr-2">
-                        <Input 
-                            type = "text"
-                            placeholder = "Enter your name"
-                            label = "Name"
-                            required
-                            initialValue={name}
-                            textChanged={event => setName(event.target.value)}
-                        />
-                    </div>
-                    <div className="w-1/2 ml-2">
-                        <Input 
-                            type = "text"
-                            placeholder = "Enter your email address"
-                            label = "Email"
-                            hint = "Your email address will not be published"
-                            initialValue={email}
-                            textChanged={event => setEmail(event.target.value)}
-                        />
-                    </div>
-                </div>
+                <PersonalInfo 
+                    name={name}
+                    nameChanged={event => setName(event.target.value)}
+                    email={email}
+                    emailChanged={event => setEmail(event.target.value)}
+                />
                 <Button 
                     text="Post Article" 
                     btnColor="bg-main-500" 
