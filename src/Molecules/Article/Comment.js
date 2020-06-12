eab4aa96574e62d1956f133f97499b4f1b12b158
joel <joel@optimiz.claims>
2020-06-12 20:18:26 +0100

    Displays nice message when they are no comments to display

--- src/Molecules/Article/Comment.js
+++ src/Molecules/Article/Comment.js
@@ -5,9 +5,10 @@ import { useHttp } from '../../Hooks/useHttp';
 import AddComment from './AddComment';
 import Para from '../../Atoms/Para';
 import likeIcon from '../../assets/icons/like.svg';
+import Spinner from '../../Atoms/Spinner/Spinner';
+import ErrorMessage from '../../Atoms/ErrorMessage';
 
 import Aux from '../../hoc/Auxilary';
-import Spinner from '../../Atoms/Spinner/Spinner';
 
 const Comment = ({articleId}) => {
 
@@ -19,26 +20,36 @@ const Comment = ({articleId}) => {
 
     let articleComments = <Spinner />
 
-    // articleComments = Object.entries(comments).map(comment => {
-    //     console.log(comment[1]);
-    //     return (
-    //         <div className="flex items-start my-5" key={comment[1].name}>
-    //             <div className="h-12 w-12 rounded-full bg-gray-300 mr-2">
+    if(commentsErr) {
+        articleComments = <ErrorMessage>Sorry, comments could not be loaded for some reason </ErrorMessage>
+    } else if(comments) {
+        articleComments = Object.entries(comments).map(comment => {
+            console.log(comment[1]);
+            return (
+                <div className="flex items-start my-5" key={comment[1].name}>
+                    <div className="h-12 w-12 rounded-full bg-gray-300 mr-2">
 
-    //             </div>
-    //             <div className="w-3/5"> 
-    //                 <p className="text-sm font-bold"> {comment[1].name}  <span className=" font-normal text-gray-400"> |  2 hours ago</span></p>
-    //                 <Para  text={comment[1].body} styles="text-sm mt-2 text-gray-400" />
-    //                 {/* <a href="/"><p className="text-sm text-gray-900">Reply</p></a> */}
-    //             </div>
-    //         </div>
-    //     );
-    // })
+                    </div>
+                    <div className="w-3/5"> 
+                        <p className="text-sm font-bold"> {comment[1].name}  <span className=" font-normal text-gray-400"> |  2 hours ago</span></p>
+                        <Para  text={comment[1].body} styles="text-sm mt-2 text-gray-400" />
+                        {/* <a href="/"><p className="text-sm text-gray-900">Reply</p></a> */}
+                    </div>
+                </div>
+            );
+        })
+    } else {
+        articleComments = <p className="text-main-200 font-semibold text-sm">Be the first to leave a comment</p>
+    }
+
+    
+
+ 
 
     return (
         <Aux>
             <div className="mt-8 flex items-center">
-                {/* <p className="text-gray-700 text-base font-bold pr-4">{comments.length} comments</p> */}
+                <p className="text-gray-700 text-base font-bold pr-4">{comments ? comments.length : '0'} comment(s)</p>
                 <div className="flex items-center cursor-pointer" onClick={() => setLikes(preLikes => preLikes + 1)}> 
                     <img src={likeIcon} alt="" className="w-4 h-4" />
                     <p className="text-main-200 text-sm px-1 font-bold">Likes</p>
